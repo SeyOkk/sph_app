d37bd67102aa652326f87292f077b9dabec9169c
SQLee <1984563911@qq.com>
2023-01-11 23:35:02 +0800

    首页组件划分并引入

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import TypeNav from '@/pages/home/TypeNav'
 
 Vue.config.productionTip = false;
+
+// 将TypeNav三级分类组件注册为全局组件
+Vue.component('TypeNav', TypeNav)
 
 new Vue({
   render: (h) => h(App),
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,15 +1,33 @@
 <template>
   <div>
-    首页
+    <TypeNav />
+
+    <ListContainer />
+
+    <Recommend />
+
+    <Rank />
+
+    <Like />
+
+    <Floor />
+
+    <Brand />
   </div>
 </template>
 
 <script>
-export default {
+import ListContainer from "./ListContainer";
+import Rank from "./Rank";
+import Recommend from "./Recommend";
+import Like from "./Like";
+import Floor from "./Floor";
+import Brand from "./Brand";
 
-}
+export default {
+  name: "Home",
+  components: { ListContainer, Recommend, Rank, Like, Floor, Brand },
+};
 </script>
 
-<style>
-
-</style>
+<style lang="less" scoped></style>
